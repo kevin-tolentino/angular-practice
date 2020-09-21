48c0b01a02a4c8b78d1f40fc12da67d44a671e6a
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-09-21 16:15:04 -0700

    added first new component server component and imported component decoratorgit status and added servercomponent in array of declarations

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //you add components in the declarations property
+    AppComponent,
+    ServerComponent
   ],
-  imports: [
+  imports: [ //allows us to add other modules
     BrowserModule
     ],
   providers: [],
---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core' //the core package gives us access to the core functionalities
+
+@Component({
+  selector: 'app-server', //the html tag that we will use the component later in other component templated
+  templateUrl: './server.component.html'
+}) // add a decorator to enhance your classes, know to TS now because we imported it
+export class ServerComponent{ //not just a normal TS class, but a component
+
+}
