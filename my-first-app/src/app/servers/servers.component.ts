ada47d96601df9c77d422b5ebce9fca743b93707
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-09-22 13:22:25 -0700

    added correct formatting for servers.component.ts to select html element

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+   selector: 'app-servers', // select by html element
+  //selector: '[app-servers]', // used to select by html attribute
+ // selector: '.app-servers', // used to select by CSS class
   template: `
   <app-server></app-server>
   <app-server></app-server>
