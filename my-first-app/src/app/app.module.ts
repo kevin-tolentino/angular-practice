04c8108f15313a69dd296907993a46a81d31e53c
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-09-21 18:12:47 -0700

    added new servers component to app which we just made

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from './server/server.component'
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component'
 
 @NgModule({
   declarations: [ //you add components in the declarations property
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [ //allows us to add other modules
     BrowserModule
