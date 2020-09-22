e9b2ef565933e3ef05407ac75ecaa4fbc7e1ee76
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-09-22 13:07:37 -0700

    added styles array property for styling

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: `
-  <app-servers></app-servers>
+  <app-server></app-server>
   <app-server></app-server>
   `, //you can change "templateURL" to "template" to put an inline html
   styleUrls: ['./servers.component.css']
