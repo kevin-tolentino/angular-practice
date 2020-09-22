497cdc17d5c74b1919c1770270fe9eebab011dae
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-09-22 12:30:05 -0700

    replaced templateURL property with template to and placed inline html

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+  <app-servers></app-servers>
+  <app-server></app-server>
+  `, //you can change "templateURL" to "template" to put an inline html
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
